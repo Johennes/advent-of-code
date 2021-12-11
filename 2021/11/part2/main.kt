f3736807737154cc5d66ee68a60d0ba525a44658
Johannes Marbach <johannes.marbach@smartmobilefactory.com>
2021-12-11 20:14:40 +0100

    Add solutions for puzzle 11

---
+++ 2021/11/part2/main.kt
@@ -0,0 +1,48 @@
+import java.io.File
+
+fun bumpAndRecordFlash(matrix: List<MutableList<Int>>, i: Int, j: Int, flashed: MutableList<Pair<Int,Int>>, skipIfZero: Boolean) {
+    if (i < 0 || i >= matrix.count() || j < 0 || j >= matrix[i].count() || skipIfZero && matrix[i][j] == 0) {
+        return
+    }
+    matrix[i][j] = (matrix[i][j] + 1) % 10
+    if (matrix[i][j] == 0) {
+        flashed.add(Pair(i, j))
+    }
+}
+
+fun main() {
+    var matrix = mutableListOf<MutableList<Int>>()
+    File("input").forEachLine {
+        matrix.add(it.toCharArray().map { Character.getNumericValue(it) }.toMutableList())
+    }
+
+    var step = 0
+
+    while (true) {
+        ++step
+        var count = 0
+        var flashed = mutableListOf<Pair<Int,Int>>()
+
+        for (i in matrix.indices) {
+            for (j in matrix[i].indices) {
+                bumpAndRecordFlash(matrix, i, j, flashed, false)
+            }
+        }
+
+        while (!flashed.isEmpty()) {
+            val (i, j) = flashed.removeFirst()
+            ++count
+            for (k in -1..1) {
+                for (l in -1..1) {
+                    bumpAndRecordFlash(matrix, i+k, j+l, flashed, true)
+                }
+            }
+        }
+
+        if (count == matrix.count() * matrix[0].count()) {
+            break
+        }
+    }
+
+    println(step) // 494
+}
